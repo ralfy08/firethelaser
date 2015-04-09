601700b1323298b036d2da79ee8025d67873c224
ralfy08 <ralfjgarcia@gmail.com>
2015-04-09 11:27:41 -0700

    added the css for index.html and index.js to create hash

---
+++ www/index.js
@@ -0,0 +1,37 @@
+/* activate sidebar */
+$('#sidebar').affix({
+  offset: {
+    top: 235
+  }
+});
+
+/* activate scrollspy menu */
+var $body   = $(document.body);
+var navHeight = $('.navbar').outerHeight(true) + 10;
+
+$body.scrollspy({
+	target: '#leftCol',
+	offset: navHeight
+});
+
+function hashFunction() {
+  var hash = "qiyh4XPJGsOZ2MEAyLkfWqeQ";
+  var hashCode = "#";
+  hashCode = hashCode.concat(hash);
+  document.getElementById("hashButton").href = hashCode;
+  window.location.href=hashCode;
+}
+
+/* smooth scrolling sections */
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000);
+        return false;
+      }
+    }
+});
